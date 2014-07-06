723d71dcb565d29962a3619c503ccd2c30b6a895
Florin Coras <fcoras@ac.upc.edu>
2014-07-06 14:27:38 +0200

    added programmating support to spin up sal-netconf-connectors
    Change-Id: I965e38ca1df5d671ecb8af298c3648bb5b246afe

--- mappingservice/netconf/src/main/java/org/opendaylight/lispflowmapping/mappingservice/netconf/LispDeviceNetconfConnector.java
+++ mappingservice/netconf/src/main/java/org/opendaylight/lispflowmapping/mappingservice/netconf/LispDeviceNetconfConnector.java
@@ -2,55 +2,35 @@ package org.opendaylight.lispflowmapping.mappingservice.netconf;
 
 import java.lang.management.ManagementFactory;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
-import javax.management.InstanceAlreadyExistsException;
-//import javax.management.InstanceNotFoundException;
-
-//import org.opendaylight.controller.config.yang.md.sal.dom.impl.DomBrokerImplModuleFactory;
-//import org.opendaylight.controller.config.yang.md.sal.dom.BrokerServiceInterface;
-
-import org.opendaylight.controller.config.yang.netty.eventexecutor.GlobalEventExecutorModuleFactory;
-import org.opendaylight.controller.config.yang.threadpool.impl.NamingThreadFactoryModuleFactory;
-import org.opendaylight.controller.config.yang.threadpool.impl.NamingThreadFactoryModuleMXBean;
-import org.opendaylight.controller.config.yang.threadpool.impl.fixed.FixedThreadPoolModuleFactory;
-import org.opendaylight.controller.config.yang.threadpool.impl.fixed.FixedThreadPoolModuleMXBean;
-import org.opendaylight.controller.config.util.ConfigTransactionJMXClient;
-import org.opendaylight.controller.config.util.ConfigRegistryJMXClient;
-import org.opendaylight.controller.config.api.LookupRegistry;
-import org.opendaylight.controller.config.yang.md.sal.binding.impl.BindingBrokerImplModuleFactory;
-//import org.opendaylight.controller.config.api.ValidationException;
-//import org.opendaylight.controller.config.api.ConflictingVersionException;
-//import org.opendaylight.controller.config.yang.md.sal.connector.netconf.NetconfConnectorModule;
-import org.opendaylight.controller.config.yang.md.sal.connector.netconf.NetconfConnectorModuleFactory;
-import org.opendaylight.controller.config.yang.md.sal.connector.netconf.NetconfConnectorModuleMXBean;
-import org.opendaylight.controller.config.yang.md.sal.dom.impl.DomBrokerImplModuleFactory;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Host;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
-//import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.connector.netconf.rev131028.modules.module.configuration.sal.netconf.connector.EventExecutor;
-//import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.connector.netconf.rev131028.modules.module.configuration.sal.netconf.connector.EventExecutorBuilder;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.opendaylight.controller.config.api.ConfigRegistry;
+import org.opendaylight.controller.config.util.ConfigRegistryJMXClient;
 
 
 
 public class LispDeviceNetconfConnector implements BundleActivator {
 	public static BundleContext bc = null;
-    final MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
-    final ConfigRegistryJMXClient configRegistryClient = new ConfigRegistryJMXClient(platformMBeanServer);
-	private NetconfConnectorModuleFactory factory;
-
+	ConfigRegistryTrackerCustomizer customizer;
+	private ServiceTracker tracker;
+	private LispNetconfConnector connector;
+	
+	
 	@Override
 	public void start(BundleContext bc) throws Exception {
 		System.out.println(bc.getBundle().getHeaders().get(Constants.BUNDLE_NAME) + " starting...");
 		LispDeviceNetconfConnector.bc = bc;
 		
-		factory = new NetconfConnectorModuleFactory();
+//		customizer = new ConfigRegistryTrackerCustomizer(bc);
+//		tracker = new ServiceTracker(bc, ConfigRegistry.class.getName(), customizer);
+//		tracker.open();
 		
-		createNetconfConnector();
+		if (connector == null)
+			connector = new LispNetconfConnector();
 	}
 
 	@Override
@@ -59,95 +39,36 @@ public class LispDeviceNetconfConnector implements BundleActivator {
 		LispDeviceNetconfConnector.bc = null;
 	}
 	
-	private void createNetconfConnector() throws InstanceAlreadyExistsException {
-        ConfigTransactionJMXClient transaction = configRegistryClient.createTransaction();
-        
-//        transaction.lookupConfigBean(moduleName, instanceName);
-        
-        String name = "testNetconf";
-        ObjectName nameCreated = transaction.createModule(factory.getImplementationName(), name);
-        NetconfConnectorModuleMXBean mxBean = transaction.newMXBeanProxy(nameCreated, NetconfConnectorModuleMXBean.class);
-        
-        mxBean.setAddress(new Host(new IpAddress(new Ipv4Address("127.0.0.1"))));
-        mxBean.setPassword("netconf");
-        mxBean.setPort(new PortNumber(830));
-        mxBean.setUsername("netconf");
-        
-        // binding-broker-osgi-registry
-        try {
-        	ObjectName bindingBrokerRegistry = createBindingBrokerRegistry(transaction);
-        	mxBean.setBindingRegistry(bindingBrokerRegistry);
-        } catch (Exception e) {
-        	
-        }
-        
-        // Broker instance
-        try {
-        	ObjectName domRegistry = createDomRegistry(transaction);
-            mxBean.setDomRegistry(domRegistry);
-        } catch (Exception e) {
-        	
-        }
-        
-        // netty-event-executor
-        try {
-        	ObjectName eventExecutor = createNettyEventExecutor(transaction);
-            mxBean.setEventExecutor(eventExecutor);
-        } catch (Exception e) {
-        	
-        }
-        
-        // threadpool
-        try {
-	        ObjectName threadpool = createThreadpool(transaction);
-	        if (threadpool != null)
-	        	mxBean.setProcessingExecutor(threadpool);
-        } catch (Exception e) {
-        	
-        }
-        
-        
-        try {
-        	transaction.commit();
-        } catch (Exception e) {
-        	
-        }
-	}
-	
-	private ObjectName createBindingBrokerRegistry(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
-		ObjectName domRegistry = transaction.createModule(BindingBrokerImplModuleFactory.NAME, "binding-osgi-broker");
-		return domRegistry;
-	}
-	
-	private ObjectName createDomRegistry(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
-		ObjectName domRegistry = transaction.createModule(DomBrokerImplModuleFactory.NAME, "dom-broker");
-		return domRegistry;
-	}
-	
-	private ObjectName createNettyEventExecutor(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
-        ObjectName eventExecutor = transaction.createModule(GlobalEventExecutorModuleFactory.NAME, "global-event-executor");
-        return eventExecutor;
-	}
-	
-	private ObjectName createThreadpool(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
-		FixedThreadPoolModuleFactory tpfactory;
-		String name = "threadpool";
-		Integer numberOfThreads = 1;
-		String prefix = name;
-		ObjectName nameCreated = null;
+
+	public class ConfigRegistryTrackerCustomizer implements ServiceTrackerCustomizer {
+		BundleContext bc;
+		LispNetconfConnector connector;
 		
-		tpfactory = new FixedThreadPoolModuleFactory();
-        nameCreated = transaction.createModule(tpfactory.getImplementationName(), name);
-        FixedThreadPoolModuleMXBean mxBean = transaction.newMXBeanProxy(nameCreated, FixedThreadPoolModuleMXBean.class);
-        mxBean.setMaxThreadCount(numberOfThreads);
+		public ConfigRegistryTrackerCustomizer(BundleContext bc) {
+			this.bc = bc;
+		}
+		
+		@Override
+		public Object addingService(ServiceReference reference) {
+			ConfigRegistry registry = (ConfigRegistry) bc.getService(reference);
+			
+			System.out.println("OMG ADDING CONFIGREGISTRY SERVICE!");
 
-        ObjectName threadFactoryON = transaction.createModule(NamingThreadFactoryModuleFactory.NAME, "naming");
-        NamingThreadFactoryModuleMXBean namingThreadFactoryModuleMXBean = transaction.newMXBeanProxy(threadFactoryON,
-                NamingThreadFactoryModuleMXBean.class);
-        namingThreadFactoryModuleMXBean.setNamePrefix(prefix);
+			connector = new LispNetconfConnector(new ConfigRegistryJMXClient(ManagementFactory.getPlatformMBeanServer()));
+			return registry;
+		}
+		
 
-        mxBean.setThreadFactory(threadFactoryON);
+		@Override
+		public void modifiedService(ServiceReference reference, Object service) {
+			// TODO Auto-generated method stub
+			
+		}
 
-		return nameCreated;
+		@Override
+		public void removedService(ServiceReference reference, Object service) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
---
+++ mappingservice/netconf/src/main/java/org/opendaylight/lispflowmapping/mappingservice/netconf/LispNetconfConnector.java
@@ -0,0 +1,208 @@
+package org.opendaylight.lispflowmapping.mappingservice.netconf;
+
+import java.lang.management.ManagementFactory;
+import java.util.Set;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import org.opendaylight.controller.config.util.ConfigRegistryJMXClient;
+import org.opendaylight.controller.config.util.ConfigTransactionJMXClient;
+import org.opendaylight.controller.config.yang.md.sal.binding.impl.BindingBrokerImplModuleFactory;
+import org.opendaylight.controller.config.yang.md.sal.connector.netconf.NetconfConnectorModuleFactory;
+import org.opendaylight.controller.config.yang.md.sal.connector.netconf.NetconfConnectorModuleMXBean;
+import org.opendaylight.controller.config.yang.md.sal.dom.impl.DomBrokerImplModuleFactory;
+import org.opendaylight.controller.config.yang.netty.eventexecutor.GlobalEventExecutorModuleFactory;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
+import org.opendaylight.controller.config.yang.threadpool.impl.flexible.FlexibleThreadPoolModuleFactory;
+import org.opendaylight.controller.config.yang.config.netconf.client.dispatcher.NetconfClientDispatcherModuleFactory;
+
+public class LispNetconfConnector {
+    private ConfigRegistryJMXClient configRegistryClient;
+    
+    private MBeanServer platformMBeanServer;
+	private NetconfConnectorModuleFactory factory;
+	
+	private ObjectName bindingBrokerRegistry;
+	private ObjectName domRegistry;
+	private ObjectName eventExecutor;
+	private ObjectName threadpool;
+	private ObjectName clientDispatcher;
+
+	public LispNetconfConnector() {
+		platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+
+		takeNap(20000);
+		
+		configRegistryClient = new ConfigRegistryJMXClient(platformMBeanServer);
+		factory = new NetconfConnectorModuleFactory();
+		
+		System.out.println("REGISTRY AND FACTORY CONSTRUCTED");
+
+		try {
+			createNetconfConnector("testNetconf");
+			createNetconfConnector("testNetconf1");
+		} catch (InstanceAlreadyExistsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void takeNap(Integer time) {
+		try {
+			System.out.println("GOING TO SLEEP " + time);
+			Thread.sleep(time);
+			System.out.println("FINISHED SLEEPING THE " + time);
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public LispNetconfConnector(ConfigRegistryJMXClient configRegistryClient) {
+		this.configRegistryClient = configRegistryClient;
+		try {
+			createNetconfConnector("testNetconf");
+			createNetconfConnector("testNetconf1");
+		} catch (Exception e) {
+			
+		}
+	}
+
+	private void createNetconfConnector(String instanceName) throws InstanceAlreadyExistsException {
+		
+		System.out.println("OMG netconf connector CALLED!");
+		
+        ConfigTransactionJMXClient transaction = configRegistryClient.createTransaction();
+        
+        if (transaction == null) {
+        	System.out.println("TRANSACTION NOT INITIALIZED!");
+        }
+        
+        String module = factory.getImplementationName();
+        ObjectName nameCreated = transaction.createModule(module, instanceName);
+        NetconfConnectorModuleMXBean mxBean = transaction.newMXBeanProxy(nameCreated, NetconfConnectorModuleMXBean.class);
+        
+        Host host = new Host("127.0.0.1".toCharArray());
+        mxBean.setAddress(host);
+        mxBean.setPassword("netconf");
+        mxBean.setPort(new PortNumber(830));
+        mxBean.setUsername("netconf");
+        mxBean.setTcpOnly(false);
+
+        solveDependencies(transaction, mxBean);
+        
+        try {
+        	transaction.commit();
+        } catch (Exception e) {
+        	
+        }
+	}
+	
+	private void solveDependencies(ConfigTransactionJMXClient transaction, NetconfConnectorModuleMXBean mxBean) {
+		
+    	bindingBrokerRegistry = findConfigBean(BindingBrokerImplModuleFactory.NAME, transaction);
+    	if (bindingBrokerRegistry != null ) {
+    		mxBean.setBindingRegistry(bindingBrokerRegistry);
+    	} else {
+    		System.out.println("NO BINDING BROKER INSTANCE");
+    	}
+
+        domRegistry = findConfigBean(DomBrokerImplModuleFactory.NAME, transaction);
+        if (domRegistry != null) {
+        	mxBean.setDomRegistry(domRegistry);
+        } else {
+        	System.out.println("NO DOM REGISTRY BROKER INSTANCE");
+        }
+        
+        eventExecutor = findConfigBean(GlobalEventExecutorModuleFactory.NAME, transaction);
+        if (eventExecutor != null) {
+            mxBean.setEventExecutor(eventExecutor);
+        } else {
+        	System.out.println("NO EVENT EXECUTOR INSTANCE");
+        }
+        
+        
+        threadpool = findConfigBean(FlexibleThreadPoolModuleFactory.NAME, transaction);
+        if (threadpool != null) {
+            mxBean.setProcessingExecutor(threadpool);
+        } else {
+        	System.out.println("NO THREADPOOL INSTANCE");
+        }
+        
+        clientDispatcher = findConfigBean(NetconfClientDispatcherModuleFactory.NAME, transaction);
+        if (threadpool != null) {
+            mxBean.setClientDispatcher(clientDispatcher);
+        } else {
+        	System.out.println("NO CLIENT DISPATCHER INSTANCE");
+        }
+        
+        
+        
+	}
+	
+	private ObjectName findConfigBean(String name, ConfigTransactionJMXClient transaction) {
+    	Set<ObjectName> set = transaction.lookupConfigBeans(name);
+    	System.out.println("Found " + set.size() + " " + name + " items!");
+    	if (set.size() > 0) {
+    		return set.iterator().next();
+    	} else {
+    		return null;
+    	}
+	}
+	
+	private ObjectName createBindingBrokerRegistry(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
+		System.out.println("Building BINDING Broker!");
+		ObjectName domRegistry = transaction.createModule(BindingBrokerImplModuleFactory.NAME, "binding-osgi-broker");
+		return domRegistry;
+	}
+	
+	private ObjectName createDomRegistry(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
+		System.out.println("Building DOM REGISTRY!");
+		ObjectName domRegistry = transaction.createModule(DomBrokerImplModuleFactory.NAME, "dom-broker");
+		return domRegistry;
+	}
+	
+	private ObjectName createNettyEventExecutor(ConfigTransactionJMXClient transaction) throws InstanceAlreadyExistsException {
+        ObjectName eventExecutor = transaction.createModule(GlobalEventExecutorModuleFactory.NAME, "global-event-executor");
+        return eventExecutor;
+	}
+	
+//	private ObjectName createThreadpool(ConfigTransactionJMXClient transaction)  {
+//		FixedThreadPoolModuleFactory tpfactory;
+//		String name = "threadpool";
+//		Integer numberOfThreads = 1;
+//		String prefix = name;
+//		ObjectName nameCreated = null;
+//		
+//        try {
+//			nameCreated = transaction.createModule(FixedThreadPoolModuleFactory.NAME, name);
+//		} catch (InstanceAlreadyExistsException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        FixedThreadPoolModuleMXBean mxBean = transaction.newMXBeanProxy(nameCreated, FixedThreadPoolModuleMXBean.class);
+//        mxBean.setMaxThreadCount(numberOfThreads);
+//
+//        ObjectName threadFactoryON = null;
+//		try {
+//			threadFactoryON = transaction.createModule(NamingThreadFactoryModuleFactory.NAME, "lisp-thread-naming");
+//		} catch (InstanceAlreadyExistsException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        NamingThreadFactoryModuleMXBean namingThreadFactoryModuleMXBean = transaction.newMXBeanProxy(threadFactoryON,
+//                NamingThreadFactoryModuleMXBean.class);
+//        namingThreadFactoryModuleMXBean.setNamePrefix(prefix);
+//
+//        mxBean.setThreadFactory(threadFactoryON);
+//
+//		return nameCreated;
+//	}
+	
+}
